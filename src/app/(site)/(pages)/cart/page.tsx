c2ac512362e2fa64225876a0c9bba06bd2c271dc
Webzworld - Perfection <support@webzworld.com>
2026-07-02 10:46:22 +0530

    removed

--- src/app/(site)/(pages)/cart/page.tsx
+++ src/app/(site)/(pages)/cart/page.tsx
@@ -3,7 +3,7 @@ import Cart from "@/components/Cart";
 
 import { Metadata } from "next";
 export const metadata: Metadata = {
-  title: "Cart Page | NextCommerce Nextjs E-commerce template",
+  title: "Cart Page | ",
   description: "This is Cart Page for NextCommerce Template",
   // other metadata
 };
--- src/app/(site)/blogs/blog-grid/page.tsx
+++ src/app/(site)/blogs/blog-grid/page.tsx
@@ -3,7 +3,7 @@ import BlogGrid from "@/components/BlogGrid";
 
 import { Metadata } from "next";
 export const metadata: Metadata = {
-  title: "Blog Grid Page | NextCommerce Nextjs E-commerce template",
+  title: "Blog Grid Page | ",
   description: "This is Blog Grid Page for NextCommerce Template",
   // other metadata
 };
